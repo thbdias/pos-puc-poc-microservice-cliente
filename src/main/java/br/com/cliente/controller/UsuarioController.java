e803543cbeca97a3e19acc435ce913ad36375ff5
thbd <thiagobalbino91@gmail.com>
2022-10-13 21:08:42 -0300

    init commit

---
+++ src/main/java/br/com/cliente/controller/UsuarioController.java
@@ -0,0 +1,23 @@
+package br.com.cliente.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping
+public class UsuarioController {
+	
+//	@Autowired
+//	private UsuarioService usuarioService;
+	
+//	@PostMapping("/save")
+//	public UsuarioDTO cadastrar(@RequestBody UsuarioDTO usuarioDTO) {
+//		return usuarioService.save(usuarioDTO);		
+//	}
+
+}
